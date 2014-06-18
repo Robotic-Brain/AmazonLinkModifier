f281f33ab2339a8d9b1270def16db6fccd896821
Robotic-Brain <github@roboticbrain.de>
2014-06-18 21:06:17 +0200

    Unit testing

--- extension/eventPage.js
+++ extension/eventPage.js
@@ -9,20 +9,56 @@
 /**
  * This array contains all the needed parameters
  */
-var wantedParameters = [
+var wantedParameters = {
     "tag": "sempervideo-21"
-];
+};
+
+/**
+ * This function turns the paramter part of an url (after "?")
+ * into a key/value map
+ * 
+ * @param string params     the url after "?"
+ * @return object           key/value map of GET parameters
+ */
+function strToMap(params) {
+    var pairs = params.split("&");
+    var result = {};
+    for (var i=0; i < pairs.length; ++i) {
+	var kv = pairs[i].split("=");
+	result[kv[0]] = kv[1];
+    }
+    return result;
+}
+
+/**
+ * This function does the reverse of strToMap
+ * and rebulds a string out of the given key/value map
+ *
+ * @param object params     the key/value map of all parameters
+ * @return string           the string of GET parameters
+ */
+function mapToStr(params) {
+    var result = "";
+    var first = true;
+    for (var key in params) {
+	if (!first) {
+	    result += "&";
+	}
+	result += key + "=" + params[key];
+    }
+    return result;
+}
 
 /**
  * This function takes the parameter part of an url
  * and checks if params contains what we want
  *
- * @param params    parameter part of url (aka. after "?")
- * @return bool     true if url looks good
+ * @param object params    parameter part of url in key/value pairs
+ * @return bool            true if url looks good
  */
 function checkIfDone(params) {
     for (var key in wantedParameters) {
-	if (getParam(params, key) !== wantedParameters[key]) {
+	if (params[key] !== wantedParameters[key]) {
 	    return false;
 	}
     }
@@ -37,15 +73,13 @@ function checkIfDone(params) {
  * @return false | string     new url or false
  */
 function checkRedirection(url) {
-    console.log("url is: "+url);
     var urlParts = url.split("?", 2);
-    var params = urlParts[1];
+    var params = strToMap(urlParts[1]);
     if (!checkIfDone(params)) {
 	for (var key in wantedParameters) {
-	    params = setParam(params, key, wantedParameters[key]);
+	    params[key] = wantedParameters[key];
 	}
-	var newUrl = urlParts[0] + "?" + params;
-	console.log(newUrl);
+	var newUrl = urlParts[0] + "?" + mapToStr(params);
 	//return newUrl;
     }
     
---
+++ extension/unitTest.js
@@ -0,0 +1,102 @@
+/**
+ * This file contains unit test code for the extension
+ * it is not needed and should be disabled in the manifest.json
+ * when releasing the extension
+ */
+
+/**
+ * This function compares two assoziative arrays
+ *
+ * @param object lh    first array
+ * @param object rh    second array
+ * @return boll        true if equal
+ */
+function arrCmp(lh, rh) {
+    if ((typeof lh) !== (typeof rh)) {
+	return false;
+    }
+    if (lh === rh) {
+	return true;
+    }
+    if ((typeof lh) === "object") {
+	for (var key in lh) {
+	    if (!arrCmp(lh[key], rh[key])) {
+		return false;
+	    }
+	}
+	for (var key in rh) {
+	    if (!arrCmp(lh[key], rh[key])) {
+		return false;
+	    }
+	}
+	return true;
+    }
+    return false;
+}
+
+/**
+ * This function just prettyfies assertion output
+ *
+ * @param any actual      result of some expression/function
+ * @param any expect      expected value
+ */
+function assert(actual, expect) {
+    var trace = (new Error()).stack;
+    if ((typeof actual) !== (typeof expect)) {
+	console.warn("Assertion type mismatch!\n\tStack: " + trace);
+	console.warn("\tActual: ");
+	console.warn(typeof actual);
+	console.warn("\tExpected: ");
+	console.warn(typeof expect);
+    } else if(!arrCmp(actual, expect)) {
+	console.error("Assertion failed:\n\tStack: " + trace);
+	console.warn("\tActual: ");
+	console.warn(actual);
+	console.warn("\tExpected: ");
+	console.warn(expect);
+    } else {
+	console.info("Assert Passed.");
+    }
+}
+
+/***********************************************************
+ *
+ * Testcases start here
+ *
+ ***********************************************************/
+
+function SelfTest() {
+    assert(true, true);    // Should pass
+    assert("true", true);  // Should type mismatch
+    assert(true, false);   // Should fail
+    
+    assert({"foo":"bar"}, {"foo":"bar"});   // Should pass
+    assert({"bar":"foo"}, {"foo":"bar"});   // Should fail
+    
+    assert({"bar":true}, {"bar":true});     // Should pass
+    assert({"bar":true}, {"foo":"bar"});    // Should fail
+}
+
+function TestStrToMap() {
+    // single
+    assert(strToMap("foo=bar"), {"foo":"bar"});
+    // multi
+    assert(strToMap("test=lol&foo=bar"), {"test": "lol", "foo": "bar"});
+}
+
+function TestMapToStr() {
+    var val = {"foo":"bar"};
+    assert(strToMap(mapToStr(val)), val);
+    val = {"test":"lol", "foo":"bar"};
+    assert(strToMap(mapToStr(val)), val);
+    val = {"test":"lol", "foo":"bar", "bla":"blub"};
+    assert(strToMap(mapToStr(val)), val);
+}
+
+function main() {
+    SelfTest();
+    TestStrToMap();
+    TestMapToStr();
+}
+
+window.onload = function(){main();};
