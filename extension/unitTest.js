85c79dc2dc9a467425d8a69d6323bc2e965a4b2f
Robotic-Brain <github@roboticbrain.de>
2014-10-18 20:20:22 +0200

    Fixing small typo

--- extension/unitTest.js
+++ extension/unitTest.js
@@ -1,5 +1,5 @@
 /**
-   Copyright (C) 2014  Robotic-Brain <chrome-ext@roboticbrain.de
+   Copyright (C) 2014  Robotic-Brain <chrome-ext@roboticbrain.de>
    
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
